0b33d7aec9c8a6085a5cb43c0517d6ca412d33f3
yekummar <yekummar@gmail.com>
2023-07-13 16:23:01 +0000

    This is my first Jenkins MVN GIT

--- hello.java
+++ hello.java
@@ -1,5 +1,7 @@
 public class hello{
 	public static void main(String[] args){
 		System.out.println("Hi This is my first Prog....");
+		System.out.println("Thin is my first git MVN Jenkins Prog...");
+
 	}
 }
